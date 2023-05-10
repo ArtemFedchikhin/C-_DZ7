e9283b709fcb4c8e34b98fd6f3786f7f2f59386e
Artem <fedchikhin@yandex.ru>
2023-05-10 21:37:35 +0300

    add dz

---
+++ Task 50/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает
+//  позиции элемента в двумерном массиве, и возвращает значение
+//   этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+
+Console.Clear();
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(0, 16);
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] ReadPosition()
+{
+    Console.Write("What element do you want: ");
+    int[] Position = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    return Position;
+}
+
+void FindElement(int[,] matrix, int[] Position)
+{
+    if(Position[0] < matrix.GetLength(0) && Position[1] < matrix.GetLength(1))
+    {
+        Console.Write($"{matrix[Position[0],Position[1]]}");
+    }
+    else
+        Console.Write("Элемента нет!");
+}
+
+Console.Write("Enter the size of your matrix: ");
+int[] Size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] Matrix = new int[Size[0], Size[1]];
+InputMatrix(Matrix);
+int[] Indexes = ReadPosition();
+FindElement(Matrix, Indexes);
---
+++ Task 52/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+//  Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+/*
+double[,] Array = new double[,] { {1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
+
+for(int i = 0; i < Array.GetLength(0); i++)
+{
+    double sum = 0;
+    for(int j = 0; j < Array.GetLength(1); j++)
+    {
+       sum += Array[i,j];
+    }
+    Console.WriteLine($"{sum / Array.GetLength(0)}\t");
+}
+*/
+
+
+Console.Clear();
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(0, 16);
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// int[] ReadPosition()
+// {
+//     Console.Write("What element do you want: ");
+//     int[] Position = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+//     return Position;
+// }
+
+void FindElement(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i,j];
+        }
+        Console.Write($"{sum / matrix.GetLength(0)} \t");
+    }
+}
+
+Console.Write("Enter the size of your matrix: ");
+int[] Size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] Matrix = new int[Size[0], Size[1]];
+InputMatrix(Matrix);
+Console.WriteLine();
+FindElement(Matrix);
+
+
+
+
+
+
+
+
